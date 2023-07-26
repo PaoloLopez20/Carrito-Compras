65f6aa673eb6d4653085d15ca002674aa6d346f5
PaoloLopez <paaolo2000@gmail.com>
2023-07-26 18:56:12 -0500

    Subir carrito

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		nameProduct: 'Zapatos Nike',
+		price: 80,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1099&q=80',
+		nameProduct: 'Reloj',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://images.unsplash.com/photo-1546868871-7041f2a55e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+		nameProduct: 'Smartwatch',
+		price: 90,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://images.unsplash.com/photo-1585386959984-a4155224a1ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80',
+		nameProduct: 'Perfume',
+		price: 50,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+		nameProduct: 'Audifonos',
+		price: 20,
+		quantity: 1,
+	},
+	{
+		id: 6,
+		img: 'https://scontent.fcix1-1.fna.fbcdn.net/v/t39.30808-6/340909727_134830762893220_6953700953888824533_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFQRjBlYeE5iSixxEe71AJpip8geLn6Q1qKnyB4ufpDWq3hYebnjnjFZUwGSv0O8tTs0hMxj3A2JEY9LVtU5rvD&_nc_ohc=w_WjeKQCFTwAX8NqIzW&_nc_ht=scontent.fcix1-1.fna&oh=00_AfCHWfA-iUUB9c8WXjDq-F5hFeogwkAnlPyrUajW_VrslQ&oe=64C4E8A3',
+		nameProduct: 'Mongolia',
+		price: 1000000,
+		quantity: 1,
+	},
+	// Aquí pueden agregar más o modificar los productos existentes
+];
